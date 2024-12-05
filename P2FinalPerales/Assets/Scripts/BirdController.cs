4ed22654b982cd59a73e40059085acf974941e7e
NP36752 <735430@student.dusd.net>
2024-12-05 10:03:35 -0800

    added the bird, flap sounds, death sound, collision.

---
+++ P2FinalPerales/Assets/Scripts/BirdController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public float upForce = 200f;
+
+    public AudioClip Death;
+    public AudioClip Flap;
+    AudioSource audioSource;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent <AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        if(isDead == false)
+        {
+            if(Input.GetMouseButtonDown (0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                PlaySound(Flap);
+            }
+        }
+    }
+    
+    void OnCollisionEnter2D()
+    {
+        isDead = true;
+        PlaySound(Death);
+    }
+    
+    
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+}
